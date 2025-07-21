4d3e62757bc291695ed442c00a8d28a927a6bb54
Mohammad Ali Naim <mohammadali.naim.official@gmail.com>
2025-07-21 04:16:02 +0300

    restaurantmapper & related fixes

---
+++ backend/src/main/java/util/mapper/RestaurantMapper.java
@@ -0,0 +1,14 @@
+package util.mapper;
+
+import dto.restaurant.RestaurantResponseDto;
+import model.Restaurant;
+
+public final class RestaurantMapper {
+    private RestaurantMapper() {}
+
+    public static RestaurantResponseDto toDto(Restaurant r) {
+        return new RestaurantResponseDto(
+                r.getId(), r.getName(), r.getAddress(), r.getPhone(),
+                r.getLogoBase64(), r.getTaxFee(), r.getAdditionalFee());
+    }
+}
